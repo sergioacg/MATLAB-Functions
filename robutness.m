55ff202eadad64775e5f241f68d032cd36f16815
Sergio Andres Castaño Giraldo <sac_ing@outlook.com>
2023-07-26 08:41:15 -0300

    Documentation

--- robutness.m
+++ robutness.m
@@ -1,107 +1,80 @@
 function [IR,dI,dImax]=robutness(MR,MN,Con,Fr,w1,w2,n,grafica,esc)
-%///////////////////////////////////////////////////////////////////////
-%//////       Por: Sergio Andres Castaño Giraldo          /////////////
-%//////       UFSC - Florianópolis -  Brasil (2015)       /////////////
-%///////////////////////////////////////////////////////////////////////
-% SISTEMA SISO
-% Grafica el indice de robustez multiplicativo de PSF ( parecido al Inverso de la funcion de
-% transferencia en el dominio frecuencial) para un proceso nominal y un
-% proceso real (dscreto o continuo) con el controlador diseñado para el
-% proceso nominal, y tambien grafica los errores de modelage por medio del
-% indice de robustez multiplicativo WI=abs(P-Pn)/abs(Pn)y tambien por medio
-% del indice maximo de robustez multiplicativo max(WI(w))
-%
-%[IR,dI,dImax]=robutness(MR,MN,Con,Fr,w1,w2,n,grafica,esc)
-%----------------- Salidas -------------------
-% IR:   Indice de Robustez
-% dI:   Incerteza Multiplicativa
-% dImax:Incerteza MAXIMA
-%----------------- Entradas -------------------
-% MR:   Modelo real
-% MN:   Modelo nominal
-% Con:  Controlador 
-% Fr:   Filtro del predictor
-% w1:   Frecuencia inferior (log10(w1))
-% w2:   Frecuencia superior (log10(w2)) Si es discreto = log10(pi/Ts)
-% n:   numero de puntos en el grafico
-% grafica:  numero binario, se coloca en 1 si se quiere graficar o en 0 si
-%           no se quiere graficar
-% esc:  Tipo de Escala Logaritmica
-%        0 si se quiere la "X" log y la "Y" lineal
-%        1 si se quiere la "X" log y la "Y" log
+% Function to calculate and plot the Multiplicative Robustness Index (MRI) for a nominal process and a real process (discrete or continuous), 
+% with the controller designed for the nominal process. It also plots the modelling errors using the Multiplicative Robustness Index, 
+% and the maximum of this index.
+% Created by Sergio Andres Castaño Giraldo
+% UFSC - Florianópolis - Brasil, 2015
+% https://controlautomaticoeducacion.com/
+% 
+%----------------- Outputs -------------------
+% IR:   Multiplicative Robustness Index
+% dI:   Multiplicative Uncertainty
+% dImax: Maximum Uncertainty
+%----------------- Inputs -------------------
+% MR:   Real Model
+% MN:   Nominal Model
+% Con:  Controller 
+% Fr:   Filter for the predictor
+% w1:   Lower frequency (log10(w1))
+% w2:   Upper frequency (log10(w2)) If it is discrete = log10(pi/Ts)
+% n:   Number of points in the plot
+% grafica:  Binary number, it is set to 1 if you want to plot or 0 if you do not want to plot
+% esc:  Type of Logarithmic Scale
+%        0 if you want "X" log and "Y" linear
+%        1 if you want "X" log and "Y" log
 %________________________________________________________________________
 
-w=logspace(w1,w2,n); %Frecuencia
+w=logspace(w1,w2,n); %Frequency
 G=MN;
 G.iodelay=0;
 
-%Respuesta en Frecuencia de las FT
-Pw = freqresp(MR,w);  
-Pnw = freqresp(MN,w);
-Gnw = freqresp(G,w);
-Cw = freqresp(Con,w);  
-Frw = freqresp(Fr,w);   
-
+%Frequency Response of the Transfer Functions
 Cw = reshape(freqresp(Con,w),n,1);    
 Gnw = reshape(freqresp(G,w),n,1); 
 Pnw = reshape(freqresp(MN,w),n,1); 
 Pw = reshape(freqresp(MR,w),n,1); 
 Frw = reshape(freqresp(Fr,w),n,1);  
 
+% Initialize the variables to store the results
 dI(1:n)=0;
 IR(1:n)=0;
 dImax(1:n)=0;
 
-%% Etapa que funciona lo comentaré para colocar producto punto
+%% Loop over the frequency points
 for i=1:n
-    %Incerteza Multiplicativa
+    % Calculate the Multiplicative Uncertainty
     dI(i)=abs((Pw(i)-Pnw(i))/(Pnw(i))); 
-    %Indice de Robustez Multiplicativa
+    % Calculate the Multiplicative Robustness Index
     IR(i)=abs((1+Cw(i)*Gnw(i))/(Cw(i)*Gnw(i)*Frw(i)));
     
-%     %Incerteza Aditiva
-%     dI(i)=abs(Pw(i)-Pnw(i)); 
-%     %Indice de Robustez Aditiva
-%     IR(i)=abs((1+Cw(i)*Gnw(i))/(Cw(i)*Frw(i)));
-    
-    %Incerteza Maxima
+    % Calculate the Maximum Uncertainty so far
     dImax(i)=max(dI(1:i)); 
 end
-%% Etapa nueva con producto punto
-%% Indice de Robustez Aditiva
-%     IR=abs((1+Cw.*Pnw)./(Cw.*Frw));
-%     dI = abs( (Pw - Pnw) );
-%% Indice de Robustez Multiplicativa
-%     dI = abs( (Pw - Pnw) ./ Pnw );
-%     IR=abs((1+Cw.*Gnw)./(Cw.*Gnw.*Frw));
-% 
-%     dImax=max(dI(1:end)');
 
-%% Graficas
+%% Check the plot settings
 if grafica~=0 && grafica~=1
     grafica=1;
-    display('el parametro grafica de entrada debe ser 0 o 1');
-    display('Se colocó en 1 por defecto para graficar');
+    display('The input parameter "grafica" must be 0 or 1');
+    display('It was set to 1 by default for plotting');
 end
 
 if esc~=0 && esc~=1
     esc=0;
-    display('el parametro esc debe ser 0 o 1');
-    display('Se colocó en 0 por defecto para graficar');
+    display('The input parameter "esc" must be 0 or 1');
+    display('It was set to 0 by default for plotting');
 end
-%% Si se desea graficar
+
+%% Plotting, if desired
     if grafica
-        % parameters for figures
-        %tamletra=12;
+        % Parameters for figures
         tamletra=28;
         if esc==0
             semilogx(w,20*log10(IR),w,20*log10(dI),w,20*log10(dImax),'LineWidth',2),grid
-            %semilogx(w,20*log10(IR),w,20*log10(dI),'LineWidth',2),grid
         end
         if esc==1
-            loglog(w,IR,w,dI,w,dImax,'LineWidth',2),grid
-            %loglog(w,IR,w,dI,'LineWidth',2),grid
+            semilogx(w,IR,w,dI,w,dImax,'LineWidth',2),grid
         end
+        % Define the legend, labels and font size
         legend('$$dP(\omega)$$','$$|\delta P(\omega)|$$','$$|\overline{\delta P(\omega)}|$$','Location','NorthWest','interpreter','latex')
         ylabel('Magnitude', 'FontSize', tamletra,'interpreter','latex');
         xlabel('Frequency', 'FontSize', tamletra,'interpreter','latex');
